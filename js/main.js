80c063653c2b26ce911a4351d44c6c4fd726ca3e
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-03-07 19:26:48 +0100

    Second Commit

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ let gameScore = 0;
 
 function setup() {
    bg = loadImage('/img/background-image.jpg');
-   title = loadImage('../img/gametitle-2.png');
+   title = loadImage('./img/gametitle-2.png');
    newGame3 = loadImage('../img/icons/settings-icon.png');
    newGame2 = loadImage('../img/icons/Icon_zen.png');
    newGame = loadImage('../img/icons/newgame.png');
